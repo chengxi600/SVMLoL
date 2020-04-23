e7d09446fcc5a6b9a18484f8f3a1680f57c5336e
Kewlwasabi <Kewlwasabi@gmail.com>
2020-04-24 04:15:37 +0800

    comments

--- classify.py
+++ classify.py
@@ -4,14 +4,25 @@ from sklearn import svm
 import data
 
 import matplotlib.pyplot as plt
-import seaborn as sns; sns.set(font_scale=1.2)
 
-#Inputs for model
+#Clarifications:
+#A feature is a parameter taken from a Champion e.g. Kill, Gold Share, CS
+#A sample is an array of features taken from a Champion
+#Training samples is an array of samples taken from LCS Spring Split 2019 used to train the model
+#Testing samples is an array of samples taken from LCS Spring Playoffs 2020 used to test the model's accuracy
+
+#indexes of features 
 params = [7, 8, 11, 16]
+#training samples and class size array
 inputs, sample_sizes = data.get_feature_vec(params)
+#scaling training samples
 inputs = data.scaled_feature_vec(inputs)
-print(inputs)
-#if type is true put 0 else 1
+#array of labels corresponding to training data
+#0: top
+#1: jg
+#2: mid
+#3: adc
+#4: sup
 type_label = data.get_labels(sample_sizes)
 
 #Fit SVM Model
@@ -19,6 +30,8 @@ model = svm.LinearSVC(max_iter=10000)
 #model = svm.SVC(gamma='scale', C=1, kernel='rbf')
 model.fit(inputs, type_label)
 
+#takes in an int corresponding to position
+#prints position
 def classify(num):
     if num == 0:
         print('Top')
@@ -31,7 +44,8 @@ def classify(num):
     elif num == 4:
         print('Support')
 
-#testing_data should be an array of testing data with label at end
+#takes in array of testing data
+#returns accuracy of model 
 def get_acc(testing_data):
     count = 0
     total = 0
@@ -46,13 +60,15 @@ def get_acc(testing_data):
 
     return count/total
 
-#appends the true class to data
+#takes in training samples and int corresponding to position
+#returns training samples where the correct position is appended at the end of each sample
 def get_testing(inputs, x):
     for params in inputs:
         params.append(x)
     return inputs
 
-#accuracy for five roles
+#takes in testing data for each position
+#prints the accuracy for each position
 def print_acc(top, jg, mid, adc, sup):
     print('Top: ' + str(get_acc(get_testing(top, 0))))
     print('Jg: ' + str(get_acc(get_testing(jg, 1))))
@@ -60,7 +76,8 @@ def print_acc(top, jg, mid, adc, sup):
     print('Adc: ' + str(get_acc(get_testing(adc, 3))))
     print('Sup: ' + str(get_acc(get_testing(sup, 4))))
 
-#visual scatter of data
+#takes in two indexes of a parameter (feature) 'a' and 'b', an array of samples, and array of class sizes
+#return a scatter plot of the two features 
 def plot_param(a, b, data_set, sample_sizes):
     fig, ax = plt.subplots()
     ax.scatter(data.get_single_feature(a, 0, data_set, sample_sizes), data.get_single_feature(b, 0, data_set, sample_sizes), color='b')
@@ -74,23 +91,25 @@ def plot_param(a, b, data_set, sample_sizes):
     plt.ylabel('Param ' + str(b))
     plt.show()
 
-#deaths vs 
+#Scatterplot of Assists vs Gold Share
 plot_param(1, 3, inputs, sample_sizes)
 plt.clf()
+#Scatterplot of Deaths vs Gold Share
 plot_param(0, 3, inputs, sample_sizes)
 
-       
+#url strings of testing data for each position 
 sup_2020_playoffs_url = "https://lol.gamepedia.com/Special:RunQuery/TournamentStatistics?TS%5Bpreload%5D=TournamentByChampionRole&TS%5Brole%5D=Support&TS%5Btournament%5D=LCS/2020%20Season/Spring%20Playoffs&pfRunQueryFormName=TournamentStatistics"
 top_2020_playoffs_url = "https://lol.gamepedia.com/Special:RunQuery/TournamentStatistics?TS%5Bpreload%5D=TournamentByChampionRole&TS%5Brole%5D=Top&TS%5Btournament%5D=NA%20Academy%20League/2020%20Season/Spring%20Playoffs&pfRunQueryFormName=TournamentStatistics"
 mid_2020_playoffs_url = "https://lol.gamepedia.com/Special:RunQuery/TournamentStatistics?TS%5Bpreload%5D=TournamentByChampionRole&TS%5Brole%5D=Mid&TS%5Btournament%5D=NA%20Academy%20League/2020%20Season/Spring%20Playoffs&pfRunQueryFormName=TournamentStatistics"
 adc_2020_playoffs_url = "https://lol.gamepedia.com/Special:RunQuery/TournamentStatistics?TS%5Bpreload%5D=TournamentByChampionRole&TS%5Brole%5D=AD%20Carry&TS%5Btournament%5D=NA%20Academy%20League/2020%20Season/Spring%20Playoffs&pfRunQueryFormName=TournamentStatistics"
 jg_2020_playoffs_url = "https://lol.gamepedia.com/Special:RunQuery/TournamentStatistics?TS%5Bpreload%5D=TournamentByChampionRole&TS%5Brole%5D=Jungle&TS%5Btournament%5D=NA%20Academy%20League/2020%20Season/Spring%20Playoffs&pfRunQueryFormName=TournamentStatistics"
 
-
+#testing samples of each position
 inputs_top = data.parse_data(top_2020_playoffs_url, params)
 inputs_jg = data.parse_data(jg_2020_playoffs_url, params)
 inputs_mid = data.parse_data(mid_2020_playoffs_url, params)
 inputs_adc = data.parse_data(adc_2020_playoffs_url, params)
 inputs_sup = data.parse_data(sup_2020_playoffs_url, params)
 
+#prints accuracy of model 
 print_acc(inputs_top, inputs_jg, inputs_mid, inputs_adc, inputs_sup)
--- data.py
+++ data.py
@@ -6,6 +6,7 @@ import numpy as np
 from decimal import Decimal
 from sklearn import preprocessing
 
+#links for data to be scraped from
 url_top = "https://lol.gamepedia.com/Special:RunQuery/TournamentStatistics?TS%5Bpreload%5D=TournamentByChampionRole&TS%5Brole%5D=Top&TS%5Bspl%5D=Yes&TS%5Btournament%5D=LCS/2020%20Season/Spring%20Season&pfRunQueryFormName=TournamentStatistics"
 url_jg = "https://lol.gamepedia.com/Special:RunQuery/TournamentStatistics?TS%5Bpreload%5D=TournamentByChampionRole&TS%5Brole%5D=Jungle&TS%5Bspl%5D=Yes&TS%5Btournament%5D=LCS/2020%20Season/Spring%20Season&pfRunQueryFormName=TournamentStatistics"
 url_mid = "https://lol.gamepedia.com/Special:RunQuery/TournamentStatistics?TS%5Bpreload%5D=TournamentByChampionRole&TS%5Brole%5D=Mid&TS%5Bspl%5D=Yes&TS%5Btournament%5D=LCS/2020%20Season/Spring%20Season&pfRunQueryFormName=TournamentStatistics"
@@ -13,51 +14,69 @@ url_ad = "https://lol.gamepedia.com/Special:RunQuery/TournamentStatistics?TS%5Bp
 url_sup = "https://lol.gamepedia.com/Special:RunQuery/TournamentStatistics?TS%5Bpreload%5D=TournamentByChampionRole&TS%5Brole%5D=Support&TS%5Bspl%5D=Yes&TS%5Btournament%5D=LCS/2020%20Season/Spring%20Season&pfRunQueryFormName=TournamentStatistics"
 urls = [url_top, url_jg, url_mid, url_ad, url_sup]
 
-#takes in url and params of data wanted
+#takes in string url and array of indexes of the parameters (features) you want from each sample on Gamepedia
+#returns an array of samples for the specified url
 def parse_data(url, params):
     #connect to website
     page = urllib.request.urlopen(url)
     soup = BeautifulSoup(page, 'html.parser')
 
-    res = []
+    samples = []
+    
+    #loop through rows in table on Gamepedia
     for row in soup.find('tbody').find_all('tr'):
+        #get list of all the cells on each row, each row being a sample
         td = row.find_all('td')
-        temp = []
+        #temporary array to store the row's features
+        sample_features = []
+        #keep track of current parameter (feature) index
         count = 0
         for cell in td:
             if count not in params:
                 count = count + 1
             else:
+                #if parameter is wanted, remove % and append cell text as a float to array of the row's features
                 x = float(cell.text.strip('%'))
-                temp.append(x)
+                sample_features.append(x)
                 count = count + 1
+        #reset count and append the sample into array of samples
         count = 0
-        res.append(temp)
-        temp = []
-    return res[3:]
-    print('parsed data')
+        samples.append(sample_features)
+        sample_features = []
+    #slice first three as they are empty rows
+    return samples[3:]
 
+#takes in array of sizes of each class
+#returns array of same size as the total samples array, where each element indicates the sample's class
 def get_labels(sample_sizes):
     labels = np.array([])
+    #iterate through each class and appends an array of a single number that represents the class to labels array
     for i in range(len(sample_sizes)):
         labels = np.append(labels, np.full((1, sample_sizes[i]), i))
     return labels
-    print('parsed labels')
 
+#takes in array of parameters (features) you want from each sample
+#returns the sample array of all five positions and array of each class' size
 def get_feature_vec(params):
     features = []
     sample_sizes = []
+    #iterate through url array and append each position's samples array together
     for url in urls:
         for row in parse_data(url, params):
             features.append(row)
         sample_sizes.append(len(parse_data(url, params)))
     return features, sample_sizes
 
+#takes in array of samples
+#returns an array where each feature is scaled to a distribution 
 def scaled_feature_vec(features):
     #mm_scaler = preprocessing.MinMaxScaler()
     #scaled = mm_scaler.fit_transform(features)
     return preprocessing.normalize(features)
 
+#takes in the index of the parameter (feature) you want (from processed param array), the int 
+#corresponding to a position, samples array, and array of class sizes
+#returns an array of features of given position
 def get_single_feature(param, position, inputs, sample_sizes):
     sample_sizes = np.cumsum(sample_sizes)
 
